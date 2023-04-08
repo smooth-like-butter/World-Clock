2eed60e34837c9ef0a4e5dfb9c140657c52a4194
Steph <stephschatz83@gmail.com>
2023-04-08 17:20:35 -0500

    Added current location function

--- javascript/index.js
+++ javascript/index.js
@@ -22,6 +22,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -31,9 +34,9 @@ function updateCity(event) {
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-        <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
-    "A"
-  )}</small></div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
       </div>`;
 }
 updateTime();
